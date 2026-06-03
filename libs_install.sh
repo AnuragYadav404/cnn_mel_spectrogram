2447b0bdec735fac5646eb63a56221fb4ef1f3ef
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2026-06-03 22:32:34 +0200

    Initial commit and Initial EDA

---
+++ libs_install.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+uv add numpy \
+pandas \
+librosa \
+torch \
+matplotlib \
+torchaudio \
+lightning \
+timm \
+scipy \
+torchmetrics \
+wandb \
+scikit-learn \
+tqdm \
+audiomentations \
+ipython
